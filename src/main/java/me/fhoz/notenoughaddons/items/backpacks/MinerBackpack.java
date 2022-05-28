0b04b1b9cf4f400eca0cb539eff984201ada7e87
ybw0014 <i@guizhanss.com>
2022-05-28 04:35:35 -0400

    为矿工背包添加更多矿石

--- src/main/java/me/fhoz/notenoughaddons/items/backpacks/MinerBackpack.java
+++ src/main/java/me/fhoz/notenoughaddons/items/backpacks/MinerBackpack.java
@@ -1,10 +1,12 @@
 package me.fhoz.notenoughaddons.items.backpacks;
 
+import io.github.thebusybiscuit.slimefun4.api.MinecraftVersion;
 import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
 import io.github.thebusybiscuit.slimefun4.api.items.ItemSetting;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
+import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
 import io.github.thebusybiscuit.slimefun4.implementation.items.backpacks.SlimefunBackpack;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
@@ -16,11 +18,9 @@ import java.util.List;
 
 public class MinerBackpack extends SlimefunBackpack {
 
-
     private final List<Material> defaultWhitelist = new ArrayList<>();
 
     private final ItemSetting<List<String>> whitelistedMaterials = new ItemSetting<>(this, "whitelisted-materials", ToStringList(getDefaultWhitelist()));
-
 
     public MinerBackpack(int size, ItemGroup itemGroup, SlimefunItemStack item, RecipeType recipeType,
                          ItemStack[] recipe) {
@@ -46,14 +46,29 @@ public class MinerBackpack extends SlimefunBackpack {
         defaultWhitelist.add(Material.DIAMOND_ORE);
         defaultWhitelist.add(Material.LAPIS_LAZULI);
         defaultWhitelist.add(Material.LAPIS_ORE);
-        defaultWhitelist.add(Material.NETHERITE_SCRAP);
-        defaultWhitelist.add(Material.ANCIENT_DEBRIS);
         defaultWhitelist.add(Material.EMERALD);
         defaultWhitelist.add(Material.EMERALD_ORE);
         defaultWhitelist.add(Material.QUARTZ);
         defaultWhitelist.add(Material.NETHER_QUARTZ_ORE);
-        defaultWhitelist.add(Material.NETHER_GOLD_ORE);
         defaultWhitelist.add(Material.GOLD_NUGGET);
+
+        if (Slimefun.getMinecraftVersion().isAtLeast(MinecraftVersion.MINECRAFT_1_16)) {
+            defaultWhitelist.add(Material.NETHERITE_SCRAP);
+            defaultWhitelist.add(Material.ANCIENT_DEBRIS);
+            defaultWhitelist.add(Material.NETHER_GOLD_ORE);
+        }
+
+        if (Slimefun.getMinecraftVersion().isAtLeast(MinecraftVersion.MINECRAFT_1_17)) {
+            defaultWhitelist.add(Material.COPPER_ORE);
+            defaultWhitelist.add(Material.DEEPSLATE_COPPER_ORE);
+            defaultWhitelist.add(Material.DEEPSLATE_COAL_ORE);
+            defaultWhitelist.add(Material.DEEPSLATE_IRON_ORE);
+            defaultWhitelist.add(Material.DEEPSLATE_DIAMOND_ORE);
+            defaultWhitelist.add(Material.DEEPSLATE_EMERALD_ORE);
+            defaultWhitelist.add(Material.DEEPSLATE_GOLD_ORE);
+            defaultWhitelist.add(Material.DEEPSLATE_LAPIS_ORE);
+            defaultWhitelist.add(Material.DEEPSLATE_REDSTONE_ORE);
+        }
 
         return defaultWhitelist;
     }
